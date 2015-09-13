db37d4ec344d120cdb2b53dc68d303ad72414d18
Christopher Desnoyers <cjdesno@mit.edu>
2015-09-13 05:17:56 -0400

    Odometry from rpm vector

--- src/odom_to_tf.cpp
+++ src/odom_to_tf.cpp
@@ -1,55 +1,86 @@
-//placeholder
-int main() {
+#include "ros/ros.h"
+#include "geometry_msgs/Vector3.h"
+#include "nav_msgs/Odometry.h"
+#include "tf/transform_broadcaster.h"
+
+class OdometryComputer {
+public:
+
+  unsigned int leftWheelRPM = 0;
+  unsigned int rightWheelRPM = 0;
+  double x = 0.0;       //m/s
+  double y = 0.0;       //m/s
+  double theta = 0.0;   //rad, where 0 is forward
+  ros::Time lastTime = ros::Time::now();
+  ros::Time currentTime = ros::Time::now();
+  double wheelRadius = 0.5; //TODO actual value
+
+  void rpmCallback(geometry_msgs::Vector3::ConstPtr& rpm) {
+    leftWheelRPM = rpm->y;
+    rightWheelRPM = rpm->z;
+  }
+
+private:
+
   ros::NodeHandle n;
   tf::TransformBroadcaster odom_broadcaster;
-  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
+  ros::Publisher odom_pub;
+  ros::Subscriber rpm_sub;
+}
 
-  float theta = 0;
-  float x = 0;
-  float y = 0;
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "odometry_publisher");
 
-  float rightWheelVelocity = 0;
-  float leftWheelVelocity = 0;
-  float steeringAngle = 0;
+  odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
+  rpm_sub = n.subscribe("odo_val", 1000, OdometryComputer::rpmCallback);
 
-  float lastTime = 1;
-  float thisTime = 2;
+  ros::Rate r(1000.0);
 
-  float wheelBaseWidth = 0.5;
+  while(n.ok()) {
+    currentTime = ros::Time::now();
+    double rightWheelVelocity = double(rightWheelRPM) * wheelRadius * 0.10472;
+    double leftWheelVelocity = double(leftWheelRPM) * wheelRadius * 0.10472;
 
-  float centerVelocity = (rightWheelVelocity + leftWheelVelocity) / 2;
-  float dt = thisTime - lastTime;
+    double dt = (currentTime - lastTime).toSec();
 
-  x += centerVelocity * dt * cos(theta);
-  y += centerVelocity * dt * sin(theta);
-  theta += (rightWheelVelocity - leftWheelVelocity) * dt / wheelBaseWidth;
+    double wheelBaseWidth = 0.5;
 
-  geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(theta);
+    double centerVelocity = (rightWheelVelocity + leftWheelVelocity) / 2;
 
-  geometry_messages::TransformStamped odom_trans;
-  odom_trans.header.stamp = current_time;
-  odom_trans.header.frame_id = "odom";
+    x += centerVelocity * dt * cos(theta);
+    y += centerVelocity * dt * sin(theta);
+    theta += (rightWheelVelocity - leftWheelVelocity) * dt / wheelBaseWidth;
 
-  odom_trans.transform.translation.x = x;
-  odom_trans.transform.translation.y = y;
-  odom_trans.transform.translation.z = 0.0;
-  odom_trans.translation.rotation = odom_quat;
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(theta);
 
-  odom_broadcaster.sendTransform(odom_trans);
+    geometry_messages::TransformStamped odom_trans;
+    odom_trans.header.stamp = current_time;
+    odom_trans.header.frame_id = "odom";
 
-  nav_msgs::Odometry odom;
-  odom.header.stamp = current_time;
-  odom.header.frame_id = "odom";
+    odom_trans.transform.translation.x = x;
+    odom_trans.transform.translation.y = y;
+    odom_trans.transform.translation.z = 0.0;
+    odom_trans.translation.rotation = odom_quat;
 
-  odom.pose.pose.position.x = x;
-  odom.pose.pose.position.y = y;
-  odom.pose.pose.position.z = 0.0;
-  odom.pose.pose.orientation = odom_quat;
+    odom_broadcaster.sendTransform(odom_trans);
 
-  odom.child_frame_id = "base_link";
-  odom.twist.twist.linear.x = centerVelocity;
-  odom.twist.twist.linear.y = 0.0;
-  odom.twist.twist.angular.z = (rightWheelVelocity - leftWheelVelocity) / wheelBaseWidth;
+    nav_msgs::Odometry odom;
+    odom.header.stamp = current_time;
+    odom.header.frame_id = "odom";
 
-  odom_pub.publish(odom);
+    odom.pose.pose.position.x = x;
+    odom.pose.pose.position.y = y;
+    odom.pose.pose.position.z = 0.0;
+    odom.pose.pose.orientation = odom_quat;
+
+    odom.child_frame_id = "base_link";
+    odom.twist.twist.linear.x = centerVelocity;
+    odom.twist.twist.linear.y = 0.0;
+    odom.twist.twist.angular.z = (rightWheelVelocity - leftWheelVelocity) / wheelBaseWidth;
+
+    odom_pub.publish(odom);
+
+    lastTime = currentTime;
+    r.sleep();
+  }
 }
