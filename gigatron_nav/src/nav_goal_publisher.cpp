8177cfabe048a42c1facba7e5fa20a8ece30eb39
Syler <syler@mit.edu>
2016-09-12 18:11:44 -0400

    Fixed .yaml file loading in nav_goal_publisher, compiles now #8

--- gigatron_nav/src/nav_goal_publisher.cpp
+++ gigatron_nav/src/nav_goal_publisher.cpp
@@ -5,60 +5,151 @@
 #include <vector>
 //Adapted From http://wiki.ros.org/navigation/Tutorials/SendingSimpleGoals
 
+#include <yaml-cpp/yaml.h>
+#include <tf/transform_datatypes.h>
+
+
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-std::vector<std::vector<double>> goals; //This will maybe work.
-//geometry_msgs::PoseStamped goals [3]; //this won't work. Need to type correctly
-ros::param::get("/route/goals",goals);
+std::string plan_frame_id;
+double waypoint_timeout;
+std::string plan_file;
+
+std::vector<geometry_msgs::PoseStamped> goals;
 int goal_counter = 0;
 
 bool loop;
-ros::param::get("route/loop",loop);
+
+
+bool LoadPlan(std::string file_name)
+{
+  ROS_INFO("Loading nav plan file %s", file_name.c_str());
+
+  YAML::Node plan = YAML::LoadFile(file_name);
+
+ //$ dump contents to debug
+  std::cout << YAML::Dump(plan) << std::endl;
+
+  if (plan["goals"])
+  {
+
+    for(unsigned int i = 0; i < plan["goals"].size(); i++)
+    {
+      geometry_msgs::PoseStamped waypoint;
+      if(plan["goals"][i]["x"])
+      {
+        waypoint.pose.position.x = plan["goals"][i]["x"].as<double>();
+      }
+      else
+      {
+        ROS_ERROR(".yaml waypoint %d has no x value",i);
+        return false;
+      }
+      if(plan["goals"][i]["y"])
+      {
+        waypoint.pose.position.y = plan["goals"][i]["y"].as<double>();
+      }
+      else
+      {
+        ROS_ERROR(".yaml waypoint %d has no y value",i);
+        return false;
+      }
+      if(plan["goals"][i]["yaw"])
+      {
+        double yaw = plan["goals"][i]["yaw"].as<double>();
+        waypoint.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
+      }
+      else
+      {
+        ROS_WARN(".yaml waypoint %d has no yaw value, using 0 yaw",i);
+        waypoint.pose.orientation = tf::createQuaternionMsgFromYaw(0.0);
+      }
+
+      waypoint.header.frame_id = plan_frame_id; 
+      waypoint.header.stamp = ros::Time::now();
+
+      goals.push_back(waypoint);
+    }
+    ROS_WARN("Done loading %lu waypoints.", plan["goals"].size());
+  }
+  else
+  {
+    ROS_ERROR("No goals in .yaml plan file, exiting");
+    return false;
+  }
+
+  if (plan["loop"]) {
+    loop = plan["loop"].as<bool>();
+  }
+  else {
+    loop = false;
+    ROS_WARN("Plan .yaml file does not specify 'loop' parameter, defaulting to no looping");
+  }
+  return true;
+
+}
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "nav_goals");
+  ros::init(argc, argv, "nav_goal_publisher");
+
+  //$ private node handle to get parameters
+  ros::NodeHandle n("~");  
+
+  n.param<std::string>("plan_frame_id", plan_frame_id, "map");
+  n.param<std::string>("plan_file", plan_file, "ERROR");
+  n.param("waypoint_timeout", waypoint_timeout, 120.0);
+
+  //$ load plan from YAML file
+  if(!LoadPlan(plan_file))
+  {
+    return false;
+  }  
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
 
   //wait for the action server to come up
-  while(!ac.waitForServer(ros::Duration(5.0))){
+  while(!ac.waitForServer(ros::Duration(5.0)))
+  {
     ROS_INFO("Waiting for the move_base action server");
   }
 
-  move_base_msgs::MoveBaseGoal goal;
-  //Define goal
-  goal.target_pose.header.frame_id = "world"; //probably want goals in world frame right? map frame?
-  goal.target_pose.header.stamp = ros::Time::now();
+  while (n.ok()) 
+  {
 
-  goal.target_pose.pose.position.x = goals[goal_counter][0];
-  goal.target_pose.pose.position.y = goals[goal_counter][1];
+    for(unsigned int i = 0; i < goals.size(); i++)
+    {
+      move_base_msgs::MoveBaseGoal goal;
+      //Define goal
 
-  /*
-  //gotta put theta somewhere
-  goal.target_pose.pose.orientation.x = 0.0; //do we care how the robot is facing at its goal? Probably so its not backwards
-  goal.target_pose.pose.orientation.y = 0.0; //how to convert to this from theta?
-  goal.target_pose.pose.orientation.z = 0.0;
-  goal.target_pose.pose.orientation.w = 0.0;
-  */
+      goal.target_pose = goals[i];
+      ROS_INFO("Sending goal %d.", i);
+      ac.sendGoal(goal);
 
-  ROS_INFO("Sending goal.");
-  ac.sendGoal(goal);
+      ac.waitForResult(ros::Duration(waypoint_timeout));
 
-  ac.waitForResult();
+      if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) 
+      {
+        ROS_INFO("Goal Reached!");
 
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-    ROS_INFO("Goal Reached!");
-    goal_counter++;
-    if (goal_counter == 3) {
-      if (loop == true) {
-        goal_counter = 0;
-      } else {
-        ros::shutdown(); //idk if this is good practice
+        //$ reset counter to zero
+        if (i == goals.size()-1) 
+        {
+          if (loop == true) 
+          {
+            i = 0;
+          } 
+          else 
+          {
+            ros::shutdown(); //idk if this is good practice
+          }
+        }
+      } 
+      else 
+      {
+        ROS_WARN("Issue reaching goal.");
       }
     }
-  } else {
-    ROS_WARN("Issue reaching goal.");
   }
 
   return 0;
